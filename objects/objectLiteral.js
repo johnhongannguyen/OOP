6cad2262d5441122f45f53d2e6f9351ec268173a
Johhanes Nguyen <johhanesnguyen@Johhaness-MacBook-Pro.local>
2021-02-22 00:43:27 -0800

    value and primitive types

--- objects/objectLiteral.js
+++ objects/objectLiteral.js
@@ -36,3 +36,21 @@ function Circle(radius,location){
 Circle.call({},1); // this is exactly the line 38
 Circle.apply({},[1,2,3,4]);
 const another = new Circle(2);
+let number = 10;
+function increase(number) {
+    number++;
+}
+
+increase(number);
+
+console.log(number);
+
+let number1 = {value:10};
+
+function increase1(number1){
+    number1.value++;
+
+}
+
+increase1(number1);
+console.log(number1);
