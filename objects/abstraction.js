bfb9291166a01d34372e0f5bc77ee485c924918e
Johhanes Nguyen <johhanesnguyen@Johhaness-MacBook-Pro.local>
2021-02-22 01:24:58 -0800

    private properties and methods

--- objects/abstraction.js
+++ objects/abstraction.js
@@ -1,12 +1,26 @@
 function Circle(){
     this.radius = radius;
+    // in order to hide it from the outside- so we set it as a variable
+    let defaultLocation = {x:0 , y:0};
     // should hide these details, expose something is essential
     // this.defaultLocation = {x:0, y:0};
     // this.computeOptimumLocation = function(){
     //     //....
     // }
+
+    // private method
+    let computeOptimumLocation = function(factor){
+
+    }
+
     this.draw = function(){
-        this.computeOptimumLocation();
+        // closure determines what variables will be accessible to an inner function, and stay there
+        // the scope is temporary and it dies
+
+        // let x,y;
+        // defaultLocation
+        // this.radius;
+        computeOptimumLocation(0.1);
         console.log('drawing');
     }
 }
