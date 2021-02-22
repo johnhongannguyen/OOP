480aa5bd7ff310785ea376a3b7431f6472df4824
Johhanes Nguyen <johhanesnguyen@Johhaness-MacBook-Pro.local>
2021-02-22 01:42:21 -0800

    getters and setters

--- objects/abstraction.js
+++ objects/abstraction.js
@@ -1,7 +1,7 @@
-function Circle(){
+function Circle(radius){
     this.radius = radius;
     // in order to hide it from the outside- so we set it as a variable
-    let defaultLocation = {x:0 , y:0};
+    let defaultLocation = { x:0 , y:0 } ;
     // should hide these details, expose something is essential
     // this.defaultLocation = {x:0, y:0};
     // this.computeOptimumLocation = function(){
@@ -9,9 +9,12 @@ function Circle(){
     // }
 
     // private method
-    let computeOptimumLocation = function(factor){
+    // let computeOptimumLocation = function(factor){
 
-    }
+    // }
+    this.getDefaultLocation = function(){
+        return defaultLocation;
+    };
 
     this.draw = function(){
         // closure determines what variables will be accessible to an inner function, and stay there
@@ -20,10 +23,22 @@ function Circle(){
         // let x,y;
         // defaultLocation
         // this.radius;
-        computeOptimumLocation(0.1);
+        // computeOptimumLocation(0.1);
         console.log('drawing');
-    }
+    };
+     Object.defineProperty(this, 'defaultLocation', {
+        get: function(){
+            return defaultLocation;
+        },
+        set: function(value){
+            if(!value.x || !value.y)
+                throw new Error('Invalid location!!');
+            defaultLocation = value;
+        }
+
+    });
 }
 
 const circle = new Circle(10);
+circle.defaultLocation = 1;
 circle.draw();
