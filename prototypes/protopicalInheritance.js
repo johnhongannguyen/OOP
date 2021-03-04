e2361cce805d445b646a2039ade1f9bc92eba3fd
Johhanes Nguyen <johhaneshongan@gmail.com>
2021-03-03 23:56:57 -0800

    protopical Inheritance

---
+++ prototypes/protopicalInheritance.js
@@ -0,0 +1,15 @@
+function Circle (radius) {
+    this.radius = radius;
+}
+
+Circle.prototype.draw = function () {
+    console.log('draw');
+}
+
+Circle.prototype.duplicate = function (){
+    console.log('duplicate');
+}
+
+function Square(){
+    
+}
