859e62a4864292e035830af9211f02fe53a7de94
Johhanes Nguyen <johhaneshongan@gmail.com>
2021-03-04 13:41:23 -0800

    creating protypical inheritance

--- prototypes/protopicalInheritance.js
+++ prototypes/protopicalInheritance.js
@@ -1,15 +1,18 @@
+function Shape() {
+
+}
+Shape.prototype.duplicate = function (){
+    console.log('duplicate');
+}
 function Circle (radius) {
     this.radius = radius;
 }
+// Circle.prototype = Object.create(Object.prototype); //Object base
+Circle.prototype = Object.create(Shape.prototype);
 
 Circle.prototype.draw = function () {
     console.log('draw');
 }
 
-Circle.prototype.duplicate = function (){
-    console.log('duplicate');
-}
-
-function Square(){
-    
-}
+const s = new Shape();
+const c = new Circle(1);
