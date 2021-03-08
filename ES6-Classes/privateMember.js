8ea25f0b6b008c3711b7f5c519827d2fd8d56c97
Johhanes Nguyen <johhaneshongan@gmail.com>
2021-03-08 10:47:20 -0800

    implementing Symbol as private member

---
+++ ES6-Classes/privateMember.js
@@ -0,0 +1,17 @@
+// ES6 symbols - to implement private properties and method
+const _radius = Symbol();
+const _draw = Symbol();
+class Circle { 
+    constructor(radius){
+        this[_radius] = radius;
+    }
+    [_draw](){
+
+    }
+
+}
+
+const c = new Circle(1);
+const key = Object.getOwnPropertySymbols(c)[0];
+
+console.log(c[key]);
