18adcf211f5eeb4ac4fdbcefee0ea0f3e07526ee
Johhanes Nguyen <johhaneshongan@gmail.com>
2021-03-08 07:14:44 -0800

    hoisting class

---
+++ ES6-Classes/hoisting.js
@@ -0,0 +1,21 @@
+// function declaration
+// function declaration are hoisted 
+// raise to the top of code
+function sayHello(){}
+// function expression - not hoisted
+// have to deal with initialized declaration
+// need semi colon 
+const sayGoodbye = function(){};
+
+// for classes 
+// no hoisting for those classes 
+// Class Declaration
+
+class Circle {
+
+}
+
+// class Expression 
+const Square = class{
+
+}
