94909166874bd7a5fe942b5d3f5c8c972d25a58f
Johhanes Nguyen <johhaneshongan@gmail.com>
2021-03-09 08:59:31 -0800

    implementing Common JS Modules

---
+++ ES6-Tooling/circle.js
@@ -0,0 +1,16 @@
+// implementation detail
+const _radius = new WeakMap();
+
+// public interface
+class Circle {
+    constructor(radius){
+        _radius.set(this,radius);
+    }
+
+    draw(){
+        console.log(`Circle with radius ${_radius.get(this)}`);
+    }
+}
+
+
+module.exports = Circle ;
